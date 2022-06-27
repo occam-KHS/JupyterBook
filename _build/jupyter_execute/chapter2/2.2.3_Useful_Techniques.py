784bb378f4a270b31ab03743dc9102baa1d1e21b
Kiha <kiha.shin@novelis.com>
2022-06-27 15:47:05 +0900

    20220627a

--- _build/jupyter_execute/chapter2/2.2.3_Useful_Techniques.py
+++ _build/jupyter_execute/chapter2/2.2.3_Useful_Techniques.py
@@ -72,6 +72,62 @@ df.groupby('grp')['v1'].apply(lambda x: x.max() - x.mean())
 df.groupby('grp')['v1'].transform(lambda x: x.max() - x.mean())
 
 
+# ### <br>pd.cut / pd.qcut    
+# 
+# 이번에는 그룹핑을 하기 위해 활용되는 pd.qcut() 혹은 pd.cut() 메소드에 대하여 알아보겠습니다. 어떤 변수를 그룹 별로 분석하고 싶습니다. 예를 들어, 섹터가 그룹이라면 groupby('섹터')['수익률'].mean() 명령으로 섹터별로 각 섹터에 속하는 종목들의 수익율 평균울 구할 수 있습니다. 하지만 그룹 변수가 없고 연속형 변수를 구간으로 나누어 그룹화하고 싶은 경우 pd.cut() 나 pd.qcut() 을 이용합니다. pd.cut 은 직접 구간을 지정해 그룹을 만들고, pd.qcut 은 분위 수를 이용하여 구간을 만듭니다.   
+# ```python   
+# pd.qcut(Series, q=10) # 십 분위수로 구간 생성
+# pd.cut(Series, bins=[a1, a2, a3]) # bins 인수를 이용하면  (a1, a2], (a2, a3]로 구간 생성
+# ```
+# 
+# np.arange(100) 을 이용하여 0 부터 99까지 값을 생성한 후 pd.qcut 와 pd.cut 을 사용 해 보겠습니다.
+
+# In[72]:
+
+
+import numpy as np
+import pandas as pd
+
+a_list = np.arange(100)
+df = pd.DataFrame({'a': a_list}) 
+df.head()
+
+
+# 10 이하의 숫자와 90 초과의 숫자는 해댱 구간이 없어서 그룹핑이 되지 않았습니다.
+
+# In[78]:
+
+
+rank = pd.cut(df['a'], bins=[10, 25, 75, 90])
+df.groupby(rank)['a'].agg(['min','max','count'])
+
+
+# bins 구간이 모든 값을 포함하도록 하기 위해서는 아래와 같이 bins 를 설정합니다. 
+
+# In[79]:
+
+
+rank = pd.cut(df['a'], bins=[-np.inf, 10, 25, 75, 90, np.inf])
+df.groupby(rank).agg(['min','max','count'])
+
+
+# 이번에는 제가 주로 활용하는 pd.qcut 입니다. pd.cut 은 bins 로 구간을 설정해야 하나,
+# qcut 는 q 인수로 분위수를 이용하여 구간을 만듭니다. 아래 결과를 보시면 q 의 역할을 아실 수 있을 것이라고 생각합니다. 
+
+# In[80]:
+
+
+rank = pd.qcut(df['a'], q=10)
+df.groupby(rank)['a'].agg(['min','max','count'])
+
+
+# In[81]:
+
+
+rank = pd.qcut(df['a'], q=5)
+df.groupby(rank)['a'].agg(['min','max','count'])
+
+
 # In[ ]:
 
 
